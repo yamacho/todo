6aedc49a1b7e2e09f21935cd7d8a39494ff0175d
yamacho <yamacho@users.noreply.github.com>
2020-03-03 17:42:46 +0900

    ファイルから読み込む処理を実装

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ let tasks = new Map();
 const fs = require('fs');
 const fileName = './tasks.json';
 
+// 同期的にファイルから復元
+try {
+    const data = fs.readFileSync(fileName, 'utf8');
+    tasks = new Map(JSON.parse(data));
+} catch (ignore) {
+    console.log(fileName + 'から復元できませんでした');
+}
+
 /*
  * タスクをファイルに保存する
  */
