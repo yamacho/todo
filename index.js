991984da98391a98d374efe062acdc0156281f49
yamacho <yamacho@users.noreply.github.com>
2020-03-03 16:45:21 +0900

    del コマンドの実装

--- index.js
+++ index.js
@@ -55,9 +55,18 @@ function donelist() {
     return Array.from(tasks).filter(isDone).map(t => t[0]);
 }
 
+/*
+ * 項目を削除する
+ * @param {string} task
+ */
+function del(task) {
+    tasks.delete(task);
+}
+
 module.exports = {
     todo,
     list,
     done,
-    donelist
+    donelist,
+    del
 };
