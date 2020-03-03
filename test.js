23724f0310049ef568c9ac220949aa7f744e414e
yamacho <yamacho@users.noreply.github.com>
2020-03-03 16:45:39 +0900

    del のテストを実装

--- test.js
+++ test.js
@@ -15,4 +15,11 @@ todo.done('鉛筆を買う');
 assert.deepEqual(todo.list(), ['ノートを買う']);
 assert.deepEqual(todo.donelist(), ['鉛筆を買う']);
 
+// del のテスト
+todo.del('ノートを買う');
+todo.del('鉛筆を買う');
+
+assert.deepEqual(todo.list(), []);
+assert.deepEqual(todo.donelist(), []);
+
 console.log('テストが正常に完了しました');
