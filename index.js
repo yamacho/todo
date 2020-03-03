5f122e8d4764aa06ffddc73c0cce4d46b549ec65
yamacho <yamacho@users.noreply.github.com>
2020-03-03 16:39:55 +0900

    list コマンドの実装

--- index.js
+++ index.js
@@ -11,4 +11,30 @@ function todo(task) {
     tasks.set(task, false);
 }
 
-module.exports = { todo };
+/*
+ * タスクと完了したかどうかが含まれる配列を受け取り、完了したかを返す
+ * @param {array} taskAndIsDonePair
+ * @return {boolean} 完了したかどうか
+ */
+function isDone(taskAndIsDonePair) {
+    return taskAndIsDonePair[1];
+}
+
+/*
+ * タスクと完了したかどうかが含まれる配列を受け取り、完了していないかを返す
+ * @param {array} taskAndIsDonePair
+ * @return {boolean} 完了していないかどうか
+ */
+function isNotDone(taskAndIsDonePair) {
+    return !isDone(taskAndIsDonePair);
+}
+
+/*
+ * TODOの一覧の配列を取得する
+ * @return {array}
+ */
+function list() {
+    return Array.from(tasks).filter(isNotDone).map(t => t[0]);
+}
+
+module.exports = { todo, list };
